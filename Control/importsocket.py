1e909b4ae2b47f1ce13b17c425556393ac615d69
JamesOngICL <73653114+JamesOngICL@users.noreply.github.com>
2022-06-07 00:12:29 +0100

    Add files via upload

--- Control/importsocket.py
+++ Control/importsocket.py
@@ -1,37 +1,31 @@
 from socket import *
-
-#s = socket(family=AF_INET, type=SOCK_DGRAM)
-s = socket(family=AF_INET, type=SOCK_STREAM)
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
-s.bind(('172.20.10.14', 5000))
+s.bind(('0.0.0.0', 12000))
 s.listen(1)
 try:
     while True:
-        (client,address) = s.accept()
-        while True:
-            
-            #print("hi")
-            #bytesAddressPair = s.recvfrom(32)
-            content = client.recvfrom(32)[0]
-            #content = bytesAddressPair[0]
-            #address = bytesAddressPair[1]
-            if len(content) == 0:
-                break
-            
-            else:
-            
-                print(content)
+        conn,address = s.accept()
 
 
-        
-        #client.sendto(str.encode("hello"), address)
-        client.send(str.encode("hello"))
+        content = conn.recvfrom(32)[0]
+        if len(content) == 0:
+            break
+            
+        else:
+            print("debug",content)
+        cmsg = "tryingfrom>"
+        # cmsg = cmsg.decode();
+        conn.send(cmsg.encode())
                 
-        print("Closing connection")
+        print("Maintaining connection")
         #s.close()
 
 except KeyboardInterrupt:
     s.shutdown(1)
     print("kwyboRD SHUT")
     s.close()
+
+
