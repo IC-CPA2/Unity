ca6a396183d6066823ac5f0eede9b8a0121f9f82
Charmaine <73358608+ccl19@users.noreply.github.com>
2022-06-06 15:58:07 +0100

    Update settings.py

--- Mars Rover/django_project/settings.py
+++ Mars Rover/django_project/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'django_project.urls'
