2726635d1a733cddc0a3f42bf77fac6957db44b4
yscamy <80154383+yscamy@users.noreply.github.com>
2022-06-19 11:59:12 +0100

    Add files via upload

--- Radar/src/main.cpp
+++ Radar/src/main.cpp
@@ -1,47 +1,12 @@
 #include <Arduino.h>
-#include <Wire.h>
- 
-//analog read
-const int Analog_channel_pin = 27;
-int ADC_VALUE = 0;
-float voltage_value = 0;
+#include <Radar.h>
 
-const byte LED_GPIO = 32; //read from A4
+Radar radar;
 
-void setup() 
-{
-Serial.begin(9500);
-pinMode(A0,INPUT);
-
-// initialize digital pin LED_GPIO as an output.
-pinMode(LED_GPIO, OUTPUT);
+void setup() {
+  radar.setup();
 }
 
-void loop() 
-{
-
-int found = 0;
-for (int i =0; i<50; i++){  
-ADC_VALUE = analogRead(Analog_channel_pin);
-// Serial.print("ADC VALUE = ");
-// Serial.println(ADC_VALUE);
-voltage_value = (ADC_VALUE * 3.3 ) / (4095);
-// Serial.print("Voltage = ");
-// Serial.print(voltage_value);
-// Serial.println(" volts");
-if (voltage_value > 2){
-  digitalWrite(LED_GPIO, HIGH);
-  Serial.println("fan detected!");
-  found = 1;
-  break;
+void loop() {
+  radar.fan_detect();
 }
-
-}
-if(found==0){
-  digitalWrite(LED_GPIO, LOW);
-  Serial.println("no fan");
-}
-
-
-}
-
