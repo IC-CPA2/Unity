20d81bee4047728b2dd7e399393a43b135bcce16
muliPuli <milan.paczai20@imperial.ac.uk>
2022-06-21 10:48:38 +0100

    added system auto files

---
+++ late-drive/.pio/build/esp32dev/FrameworkArduino/USB.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/USB.cpp.o: \
+ C:/Users/pacza/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
+ C:/Users/pacza/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ C:/Users/pacza/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qspi_qspi/include/sdkconfig.h
