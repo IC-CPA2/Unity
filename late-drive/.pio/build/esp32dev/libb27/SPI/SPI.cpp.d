b9ad8dbe16577694faa9cc61d459ce8fd022cd6a
muliPuli <milan.paczai20@imperial.ac.uk>
2022-06-21 13:57:20 +0100

    ignore update

---
+++ late-drive/.pio/build/esp32dev/libb27/SPI/SPI.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32dev/libb27/SPI/SPI.cpp.o: \
+ C:/Users/pacza/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.cpp \
+ C:/Users/pacza/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.h \
+ C:/Users/pacza/.platformio/packages/framework-arduinoespressif32/variants/esp32/pins_arduino.h \
+ C:/Users/pacza/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-spi.h
