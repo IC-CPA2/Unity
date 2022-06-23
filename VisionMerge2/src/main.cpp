cbe5a791da3bd148a7ac1013419fe3647132a337
muliPuli <milan.paczai20@imperial.ac.uk>
2022-06-23 11:52:45 +0100

    adjusted turning parameter

--- VisionMerge2/src/main.cpp
+++ VisionMerge2/src/main.cpp
@@ -8,10 +8,10 @@
 #include <iostream>
 #include <WiFi.h>
 
-#define HSPI_MISO   12
-#define HSPI_MOSI   13
-#define HSPI_SCLK   14
-#define HSPI_SS     15
+#define HSPI_MISO 12
+#define HSPI_MOSI 13
+#define HSPI_SCLK 14
+#define HSPI_SS 15
 
 #define SCK 18
 #define MISO 19
@@ -48,33 +48,35 @@ int coordx = 0;
 int coordy = 0;
 int anglepoint = 0;
 
-//Specify which corner rover starts and distance
+// Specify which corner rover starts and distance
 
 Drive driveUnity;
 Motors motor;
 Radar radar;
 
-void initWiFi(){
-/*
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.print("connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print('.');
-    delay(1000);
-  }
-  */
+void initWiFi()
+{
+  /*
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.print("connecting to WiFi ..");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      Serial.print('.');
+      delay(1000);
+    }
+    */
 }
 
-SPIClass * hspi = NULL;
+SPIClass *hspi = NULL;
 
-void setup(){
+void setup()
+{
   Serial.begin(9600);
   driveUnity.setup();
   pinMode(HSPI_SS, OUTPUT);
   hspi = new SPIClass(HSPI);
-  hspi->begin(HSPI_SCLK,HSPI_MISO,HSPI_MOSI,HSPI_SS);
+  hspi->begin(HSPI_SCLK, HSPI_MISO, HSPI_MOSI, HSPI_SS);
   radar.setup();
   spi_returnval = 0;
   mfrc522.PCD_Init();
@@ -84,95 +86,113 @@ void setup(){
   spi_returnval = 0;
 }
 
-char* getColor(char* string){
-  char * token = strtok(string, ";");
+char *getColor(char *string)
+{
+  char *token = strtok(string, ";");
   return token;
 }
 
-int getDist(String string){
+int getDist(String string)
+{
   int index = string.indexOf(';');
-  String sub_SC = string.substring(0,index);
-  string.remove(0,sub_SC.length()+1);
+  String sub_SC = string.substring(0, index);
+  string.remove(0, sub_SC.length() + 1);
   int d = string.indexOf(';');
-  String sub_SD = string.substring(0,d);
+  String sub_SD = string.substring(0, d);
   return sub_SD.toInt();
 }
 
-int getPos(char* string){
+int getPos(char *string)
+{
   int pos = 0;
-  char* token = strtok(string, ";");
+  char *token = strtok(string, ";");
   char arr[3][50];
   int i = 0;
-  
-  while(token != NULL){
+
+  while (token != NULL)
+  {
     strcpy(arr[i], token);
-		token = strtok(NULL, ";");
+    token = strtok(NULL, ";");
     i++;
-	}
+  }
   pos = atoi(arr[2]);
   return pos;
 }
 
-int retlargestbin(int arr[65]){
+int retlargestbin(int arr[65])
+{
   int maxindex = 0;
   int maxval = 0;
-  for(int i=0;i<65;i++){
-    if(arr[i]>maxval){
+  for (int i = 0; i < 65; i++)
+  {
+    if (arr[i] > maxval)
+    {
       maxval = arr[i];
       maxindex = i;
     }
   }
-  if (maxindex == 0 || maxval == 0){
+  if (maxindex == 0 || maxval == 0)
+  {
     return 1000;
   }
-  else{
-    return 2670/(maxindex*10);
+  else
+  {
+    return 2670 / (maxindex * 10);
   }
 }
 
-int totlum(){
+int totlum()
+{
   int tot = 1;
   int c = 1;
-  for(int i=0;i<1000;i++){
+  for (int i = 0; i < 1000; i++)
+  {
     hspi->beginTransaction(settings);
     digitalWrite(HSPI_SS, LOW);
-    spi_val=hspi->transfer16(spi_returnval);
+    spi_val = hspi->transfer16(spi_returnval);
     spi_returnval = 0;
     digitalWrite(HSPI_SS, HIGH);
     hspi->endTransaction();
-    
+
     int col = spi_val % 8;
     spi_val = spi_val / 8;
     int pos = spi_val % 8;
     spi_val = spi_val / 8;
     int dist = spi_val;
 
-    if(col==0){
+    if (col == 0)
+    {
       c++;
       tot = tot + dist;
     }
   }
-  return tot/c;
+  return tot / c;
 }
 
-int retstrength(int arr[65]){
+int retstrength(int arr[65])
+{
   int maxindex = 0;
   int maxval = 0;
-  for(int i=0;i<65;i++){
-    if(arr[i]>maxval){
+  for (int i = 0; i < 65; i++)
+  {
+    if (arr[i] > maxval)
+    {
       maxval = arr[i];
       maxindex = i;
     }
   }
-  if (maxindex == 0 || maxval == 0){
+  if (maxindex == 0 || maxval == 0)
+  {
     return 0;
   }
-  else{
+  else
+  {
     return maxval;
   }
 }
 
-String currentview(){
+String currentview()
+{
   int redbin[65] = {0};
   int bluebin[65] = {0};
   int pinkbin[65] = {0};
@@ -212,43 +232,51 @@ String currentview(){
     int pos = spi_val % 8;
     spi_val = spi_val / 8;
     int dist = spi_val;
-    int tdist = dist/10;
+    int tdist = dist / 10;
 
-    if (col == 0 && pos == 0 && dist == 0){
-      //driveUnity.brake();
+    if (col == 0 && pos == 0 && dist == 0)
+    {
+      // driveUnity.brake();
       currlength = 0;
-      //cornerleft = 1;
+      // cornerleft = 1;
     }
 
-    if (col == 0){
+    if (col == 0)
+    {
       nocol++;
     }
-    else if (col == 1){
+    else if (col == 1)
+    {
       redbin[tdist]++;
       redpos = redpos + pos;
       redc++;
     }
-    else if (col == 2){
+    else if (col == 2)
+    {
       bluebin[tdist]++;
       bluepos = bluepos + pos;
       bluec++;
     }
-    else if (col == 3){
+    else if (col == 3)
+    {
       yelbin[tdist]++;
       yelpos = yelpos + pos;
       yelc++;
     }
-    else if (col == 4){
+    else if (col == 4)
+    {
       pinkbin[tdist]++;
       pinkpos = pinkpos + pos;
       pinkc++;
     }
-    else if (col == 5&&tdist<=65){
+    else if (col == 5 && tdist <= 65)
+    {
       gbin[tdist]++;
       gpos = gpos + pos;
       gc++;
     }
-    else if (col == 6&&tdist<=65){
+    else if (col == 6 && tdist <= 65)
+    {
       dgbin[tdist]++;
       dgpos = dgpos + pos;
       dgc++;
@@ -274,35 +302,46 @@ String currentview(){
           "bluedist: "+String(bluedistf) + + "," + String(retstrength(bluebin))+ ","+ String(bluec)+";"+
           "gdistf: "+String(gdistf) + + "," + String(retstrength(gbin))+ ","+ String(gc)+";"+
           "dgdistf: "+String(dgdistf) + + "," + String(retstrength(dgbin))+ ","+ String(dgc)+";"+
-          "buildingdist: "+String(buildingcount)+","+String(buildingdist); 
+          "buildingdist: "+String(buildingcount)+","+String(buildingdist);
   */
-  if(retstrength(redbin)>5&&redc>20&&retstrength(pinkbin)<100){
-    return "r;"+String(reddistf)+";"+String(redpos/redc);
+  if (retstrength(redbin) > 5 && redc > 20 && retstrength(pinkbin) < 100)
+  {
+    return "r;" + String(reddistf) + ";" + String(redpos / redc);
   }
-  else if(retstrength(yelbin)>10&&retstrength(pinkbin)<100){
-    return "y;"+String(yeldistf)+";"+String(yelpos/yelc);
+  else if (retstrength(yelbin) > 10 && retstrength(pinkbin) < 100)
+  {
+    return "y;" + String(yeldistf) + ";" + String(yelpos / yelc);
   }
-  else if(retstrength(pinkbin)>20){
-    return "p;"+String(pinkdistf)+";"+String(pinkpos/pinkc);
+  else if (retstrength(pinkbin) > 20)
+  {
+    return "p;" + String(pinkdistf) + ";" + String(pinkpos / pinkc);
   }
-  else if(retstrength(bluebin)>10&&bluec>=10){
-    return "b;"+String(bluedistf)+";"+String(bluepos/bluec);
+  else if (retstrength(bluebin) > 10 && bluec >= 10)
+  {
+    return "b;" + String(bluedistf) + ";" + String(bluepos / bluec);
   }
-  else if(retstrength(dgbin)>10&&dgc>15){
-    return "dg;"+String(dgdistf)+";"+String(dgpos/dgc);
+  else if (retstrength(dgbin) > 10 && dgc > 15)
+  {
+    return "dg;" + String(dgdistf) + ";" + String(dgpos / dgc);
   }
-  else if(retstrength(gbin)>10&&gc>15){
-    return "lg;"+String(gdistf)+";"+String(gpos/gc);
+  else if (retstrength(gbin) > 10 && gc > 15)
+  {
+    return "lg;" + String(gdistf) + ";" + String(gpos / gc);
   }
-  else{
+  else
+  {
     return "nb;1000;1000";
   }
 }
 
-void loop(){
+void loop()
+{
   Serial.println(totlum());
   Serial.println(getDist(currentview()));
-  driveUnity.turn(105,false);
+  driveUnity.turn(90, false);
+  delay(1000);
+  driveUnity.turn(90, true);
+  delay(1000);
   /*
   Serial.println(currentview());
   String view = currentview();
